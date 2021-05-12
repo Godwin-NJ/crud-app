86e18ad165b066c423e5f86742db3536b55dd650
Godwin Amadi JNR <amadigodwin7@gmail.com>
2021-05-12 22:52:45 +0100

    commit

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, './Views'));
 app.use(cors());
 
 
