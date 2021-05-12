28491808c4d35a5e967cac3e55ae1fe953a139b8
Godwin Amadi JNR <amadigodwin7@gmail.com>
2021-05-12 14:47:18 +0100

    second commit

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
-let port = process.env.PORT || 3000
+
+
 
 const app = express();
+
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
