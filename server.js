db8139339ad898e206995dc27c62843df47396b4
Godwin Amadi JNR <amadigodwin7@gmail.com>
2021-05-13 08:20:32 +0100

    commit

--- server.js
+++ server.js
@@ -95,7 +95,20 @@ MongoClient.connect(
 
 
 
+app.use((req, res, next) => {
+ const error = new Error('Not found');
+ error.status = 404;
+ next(error);
+});
 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).send({
+      error: {
+        status: error.status || 500,
+        message: error.message || 'Internal Server Error',
+      },
+    });
+  });
 
 app.listen(port, () => {
   console.log("server running on port 3000");
